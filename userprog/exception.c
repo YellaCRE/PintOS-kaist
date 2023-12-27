888993683b3795f69b665748caa3d1fd6de5c2b8
YellaCRE <hchj2005@gmail.com>
2023-12-27 06:47:19 +0000

    feat: include 빠진 것 추가

--- userprog/exception.c
+++ userprog/exception.c
@@ -5,6 +5,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "intrinsic.h"
+#include "userprog/syscall.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
